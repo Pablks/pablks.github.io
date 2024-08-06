ba57d26553912accaf27b1fc196423f937c58965
Pablks <122682171+Pablks@users.noreply.github.com>
2024-08-06 23:23:41 +0100

    Add files via upload

---
+++ notes/index.js
@@ -0,0 +1,21 @@
+const icon = document.getElementById("icon");
+// const counter = document.getElementById("counter");
+// const textarea = document.getElementById("textarea");
+
+textarea.addEventListener("input", updateCounter());
+
+counter.addEventListener("click", () => {
+    counter.dataset.mode = counter.dataset.mode == "word" ? "char" : "word";
+    updateCounter();
+})
+
+function updateCounter() {
+    const text = textarea.value;
+    let counterMode = counter.dataset.mode;
+    let wordCount = text.split(/\s+/).length;
+    let charCount = text.length;
+    if (counterMode == "word")
+        counter.innerText = `${wordCount} words`;
+    else
+        counter.innerText = `${charCount} characters`;
+}
